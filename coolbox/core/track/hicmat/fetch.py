7f0522636d3fbe14fcb5d4d3257fecde956ae263
nanguage <nanguage@yahoo.com>
2020-12-04 22:13:58 +0800

    add hiccups expection normalize

--- coolbox/core/track/hicmat/fetch.py
+++ coolbox/core/track/hicmat/fetch.py
@@ -1,8 +1,10 @@
+import re
 import abc
 
 import numpy as np
 from scipy.linalg import toeplitz
 from scipy.ndimage import gaussian_filter
+from scipy.signal import convolve2d
 
 from coolbox.utilities.logtools import get_logger
 
@@ -66,14 +68,7 @@ class FetchHiC(abc.ABC):
 
         self.fetched_binsize = wrap.fetched_binsize  # expose fetched binsize
 
-        # fill zero and nan with small value
-        small = self.SMALL_VALUE
-        zero_indices = arr == 0
-        nan_indices = np.isnan(arr)
-        self.zero_indices = zero_indices
-        self.nan_indices = nan_indices
-        arr[zero_indices] = small
-        arr[nan_indices] = small
+        arr = self.fill_zero_nan(arr)
 
         # process the matrix
         if 'transform' in self.properties and self.properties['transform'] != 'no':
@@ -98,7 +93,48 @@ class FetchHiC(abc.ABC):
                 raise ValueError(
                     "process_func should a one argument function "
                     "receive a matrix return a processed matrix.")
+
         return arr
+
+    def fill_zero_nan(self, arr):
+        # fill zero and nan with small value
+        small = self.SMALL_VALUE
+        zero_indices = arr == 0
+        nan_indices = np.isnan(arr)
+        self.zero_indices = zero_indices
+        self.nan_indices = nan_indices
+        arr[zero_indices] = small
+        arr[nan_indices] = small
+        return arr
+
+    @staticmethod
+    def diagonal_mean(mat):
+        return [np.diagonal(mat, i).mean() for i in range(mat.shape[0])]
+
+    @staticmethod
+    def diagonal_mean_std(mat):
+        means = []
+        stds = []
+        for i in range(mat.shape[0]):
+            diagonal = np.diagonal(mat, i)
+            means.append(diagonal.mean())
+            stds.append(diagonal.std())
+        stds = np.array(stds)
+        stds[stds == 0] = stds[stds > 0].min()
+        return means, stds
+
+    @staticmethod
+    def __donut_kernel(p, w):
+        k1 = np.ones((2 * w + 1, 2 * w + 1))
+        k2 = np.zeros((2 * w + 1, 2 * w + 1))
+        k2[w - p:w + p + 1, w - p:w + p + 1] = 1
+        k3 = np.zeros((2 * w + 1, 2 * w + 1))
+        k3[:w - p, w] = 1
+        k3[w, :w - p] = 1
+        k3[w, w + 2 * p - 1:2 * w + 1] = 1
+        k3[w + 2 * p - 1:2 * w + 1, w] = 1
+        k = k1 - k2 - k3
+        return k
 
     def __normalize_matrix(self, mat):
         norm_mth = self.properties['normalize']
@@ -108,21 +144,34 @@ class FetchHiC(abc.ABC):
             if total != 0:
                 res = mat / total
         elif norm_mth == 'expect':
-            means = [np.diagonal(mat, i).mean() for i in range(mat.shape[0])]
+            means = self.diagonal_mean(mat)
             expect = toeplitz(means)
             res = mat / expect
         elif norm_mth == 'zscore':
-            means = []
-            stds = []
-            for i in range(mat.shape[0]):
-                diagonal = np.diagonal(mat, i)
-                means.append(diagonal.mean())
-                stds.append(diagonal.std())
-            stds = np.array(stds)
-            stds[stds == 0] = stds[stds > 0].min()
+            means, stds = self.diagonal_mean_std(mat)
             mat_mean = toeplitz(means)
             mat_std = toeplitz(stds)
             res = (mat - mat_mean) / mat_std
+        elif re.match("hiccups:.:.", norm_mth):
+            p, w = norm_mth.strip("hiccups:").split(":")
+            p, w = int(w), int(p)
+            kernel = self.__donut_kernel(p, w)
+
+            def apply_donut(m):
+                m_ext = np.zeros((m.shape[0] + 2 * (w - 1), m.shape[0] + 2 * (w - 1)))
+                idx_center = slice(w, w+m.shape[0]), slice(w, w+m.shape[1])
+                m_ext[idx_center] = m
+                m_f = convolve2d(m_ext, kernel, mode='same')
+                m_f = m_f[idx_center]
+                return m_f
+
+            means = self.diagonal_mean(mat)
+            exp_decay = toeplitz(means)
+            m_donut = apply_donut(mat)
+            exp_donut = apply_donut(exp_decay)
+            exp = (m_donut / exp_donut) * exp_decay
+            res = mat / exp
+
         return res
 
     def __transform_matrix(self, arr):
